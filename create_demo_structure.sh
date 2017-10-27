4d4a4cf3bd90793d07cc053e94cce50d433f18ec
Daniel Huguenin <huguenin@getyourguide.com>
2017-10-27 11:21:34 +0200

    Add script to create demo structure

---
+++ create_demo_structure.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+git checkout -b C1
+git push -fu origin C1
+
+git checkout C1
+git checkout -b C2
+touch C2.txt
+git add C2.txt
+git commit -m "Commit message of C2"
+git push -fu origin C2
+
+git checkout C2
+git checkout -b C4
+touch C4.txt
+git add C4.txt
+git commit -m "Commit message of C4"
+git push -fu origin C4
+
+git checkout C2
+git checkout -b C3
+touch C3.txt
+git add C3.txt
+git commit -m "Commit message of C3"
+git push -fu origin C3
+
+git checkout C4
+git checkout -b C5
+touch C5.txt
+git add C5.txt
+git commit -m "Commit message of C5"
+git push -fu origin C5
+
+git checkout C5
+git checkout -b C6
+git merge --no-edit C3
+git push -fu origin C6
